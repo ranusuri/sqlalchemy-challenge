86819d77a9487ce03f046bb5e56eb433d6a648a8
Radhika Anusuri <rad.guttu@gmail.com>
2022-03-11 08:48:13 -0500

    completed all tasks

--- climate_app.py
+++ climate_app.py
@@ -81,6 +81,66 @@ def stations():
 
     return jsonify(stations_ls)
 
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+     # Find the most recent date in the data set.
+    recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first().date
+
+    # Calculate the date one year from the last date in data set.
+    date_one_year_ago =  dt.datetime.strptime(recent_date, '%Y-%m-%d')  - dt.timedelta(365)
+
+    # Query the last 12 months of temperature observation data for this station and plot the results as a histogram
+    temp_obs = session.query(Measurement.date, Measurement.tobs).\
+            filter(Measurement.date >= date_one_year_ago).\
+            group_by(Measurement.date).all()
+
+    # Convert list of tuples into normal list
+    temp_obs_list = list(np.ravel(temp_obs))
+
+    session.close()
+
+    return jsonify(temp_obs_list)
+
+
+    
+@app.route("/api/v1.0/<start>")
+def display_temperature_start(start):
+    print("in display temperature start def")
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    select = [func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)]
+    temperatures = session.query(*select).\
+                filter(Measurement.date >= start).all()
+
+    # Convert list of tuples into normal list
+    temperatures_list = list(np.ravel(temperatures))
+
+    session.close()
+
+    return jsonify(temperatures_list)
+
+@app.route("/api/v1.0/<start>/<end>")
+def display_temperature_start_end(start,end):
+    print("in display temperature start end")
+    print(start)
+    print(end)
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    select = [func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)]
+    temperatures = session.query(*select).\
+              filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+
+
+    # Convert list of tuples into normal list
+    temperatures_list = list(np.ravel(temperatures))
+
+    session.close()
+
+    return jsonify(temperatures_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
